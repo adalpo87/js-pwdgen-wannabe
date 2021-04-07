b58ea37d2d97b2876bade55ab923f68dc5feed9a
adalpo87 <adalpo87@gmail.com>
2021-04-07 09:35:26 +0200

    corretto

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,6 @@ var nome = prompt("Ciao, come ti chiami?");
 var cognome = prompt("E il tuo cognome?");
 var colore = prompt("Mi puoi dire il tuo colore preferito?");
 
-var risposta = "E' un piacere conoscerti " + nome + " " + cognome + ". So che il tuo colore preferito è il " + colore + ".";
+var risposta = nome + cognome + colore + "21";
 
 document.getElementById('identita').innerHTML = risposta;
